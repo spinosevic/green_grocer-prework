cc69e618284fb33e3bf2c61787dc09869b017272
Dario Spina <dario_spina01@live.it>
2018-09-23 08:29:32 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,25 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  new_hash=cart.uniq
+  new_hash.each do |array|
+    array.each do |item,details|
+      cart.each do |array_with_repetition|
+       array_with_repetition.each do |item2,details2|
+       count=0
+          if !array[item].has_key?(:count)
+            count=0
+            array[item][:count]=count
+          
+          elsif item2==item
+            count=count+1
+            array[item][:count]=count
+            end
+        end
+      end
+  end
+end
+binding.pry
+return new_hash
 end
 
 def apply_coupons(cart, coupons)
