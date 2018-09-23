b4fc5ec5535d9dc6c1af80b3f655bae8917d013b
Dario Spina <dario_spina01@live.it>
2018-09-23 09:37:20 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,5 @@
 require 'pry'
 def consolidate_cart(cart)
-<<<<<<< HEAD
   new_hash=cart.uniq
   new_hash.each do |array|
     array.each do |item,details|
@@ -10,36 +9,15 @@ def consolidate_cart(cart)
           if !array[item].has_key?(:count)
             count=0
             array[item][:count]=count
-          
-          elsif item2==item
+          end
+          if item2==item
             count=count+1
             array[item][:count]=count
             end
-=======
-  binding.pry
-  new_hash=cart.uniq
- 
-  new_hash.each do |array|
-    array.each do |item,details|
-      cart.each do |array_with_repetition|
-        
-        array_with_repetition.each do |item2,details2|
-          binding.pry
-          if item2==item
-            count+=1
-            array[item][:count]=count
-   
           end
->>>>>>> 2aedf0c0e9b514f8a509e86275d2e29c2fcc1d7f
         end
       end
-  end
-end
-<<<<<<< HEAD
-binding.pry
-=======
-
->>>>>>> 2aedf0c0e9b514f8a509e86275d2e29c2fcc1d7f
+    end
 return new_hash
 end
 
